4db770f74d8142953323221b92477fcb32ba17d0
Jefferson Pulido <jeffersonpulido55@gmail.com>
2023-06-05 08:36:07 -0500

    variables en Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@
 function App() {
 
   return (
-    <h1 className="text-5xl font-bold underline text-center">Hello world!</h1> 
+    <h1 className="text-5xl underline text-center text-red-500">
+      Hello world! <span className="text-darkGreen font-fontTitleBold">PRUEBA</span>
+    </h1> 
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'orange': '#ff5a23',
+        'darkGreen': '#03624e',
+        'lightGreen': '#00b288',
+        'redBlood': '#9e1b0e'
+      },
+      fontFamily: {
+        fontGeneral: ['Quicksand', 'sans-serif'],
+        fontTitleRegular: ['FontSpringRegular', 'serif'],
+        fontTitleBold: ['FontSpringBold', 'serif'],
+      }
+    },
   },
   plugins: [],
 }
