9cbb676f1758233257c5eb647d6d175128050751
María F. Herrera G <mariaf.herrera@outlook.com>
2023-06-12 17:57:26 -0500

    V0.1 Tailwind Config

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,21 @@
 function App() {
 
   return (
-    <h1 className="text-5xl underline text-center text-red-500">
-      Hello world! <span className="text-darkGreen font-fontTitleBold">PRUEBA</span>
-    </h1> 
+    <section>
+      <h1 className="text-5xl underline text-center text-orange-800">
+        Hello world! <span className="text-green-600 font-fontTitleBold">PRUEBA</span>
+      </h1>
+      <div>
+        <h1 className="h1">Big Title</h1>
+        <h2 className="h2">Medium Title</h2>
+        <h3 className="h3">Subtitle</h3>
+        <h4 className="h4">Highlights</h4>
+        <p1 className="p1">Big text</p1>
+        <p2 className="p2">Medium text</p2>
+        <p3 className="p3">Small text</p3>
+      </div> 
+      <button className="green-button h3">¡Regístrate!</button>
+    </section>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,18 @@ export default {
   theme: {
     extend: {
       colors: {
-        'orange': '#ff5a23',
-        'darkGreen': '#03624e',
-        'lightGreen': '#00b288',
-        'redBlood': '#9e1b0e'
+        'orange': {'200': '#ffc9a8', '500': '#ff5a23', '800': '#9e1b0e',},
+        'lightOrange': {'50': '#fff9f6',},
+        'green': {'100': '#d0fbe8', '200': '#a4f6d6', '600': '#00b288', '900': '#045042',},
+        'whiteGreen': {'50': '#fcfefe',},
+        'lightGreen': {'50': '#e5faf1',},
+        'gray': {'200': '#e4e4e4', '600': '#818181', '950': '#282828',},
       },
       fontFamily: {
         fontGeneral: ['Quicksand', 'sans-serif'],
         fontTitleRegular: ['FontSpringRegular', 'serif'],
         fontTitleBold: ['FontSpringBold', 'serif'],
-      }
+      },
     },
   },
   plugins: [],
