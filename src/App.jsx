e1c57101911b5e3c47188f679b6f04aaeea44d4e
manuel101284 <manuel101284@gmail.com>
2023-06-22 00:05:52 -0500

    V0.3 Components: CreationAccount, InformationProfile, SellProducts

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
 import Navbar from "./components/Navbar";
 import CreateAccount from "./components/CreateAccount"
 import InformationProfile from "./components/InformationProfile";
+import Footer from "./components/Footer";
+import SellProducts from "./components/SellProducts";
 
 function App() {
 
   return (
-    <>
+    <section>
       <Navbar />
       <h1 className="text-5xl underline text-center text-red-500">
         Hello world! <span className="text-darkGreen font-fontTitleBold">PRUEBA</span>
       </h1>
+      <div>
+        <h1 className="h1">Big Title</h1>
+        <h2 className="h2">Medium Title</h2>
+        <h3 className="h3">Subtitle</h3>
+        <h4 className="h4">Highlights</h4>
+        <p1 className="p1">Big text</p1>
+        <p2 className="p2">Medium text</p2>
+        <p3 className="p3">Small text</p3>
+      </div>
+      <button className="green-button h3">¡Regístrate!</button>
       <br />
-      {/* <CreateAccount /> */}
+
+      {/* <CreateAccount />
       <InformationProfile />
-    </>
+      <SellProducts /> */}
+
+      <Footer />
+    </section>
   );
 }
 
--- src/components/CreateAccount.jsx
+++ src/components/CreateAccount.jsx
@@ -16,25 +16,25 @@ const createAccount = () => {
 
             <div className='flex flex-col text-center sm:text-sm lg:text-lg xl:text-xl'>
                 <p className='mt-6 pt-2 text-[#03624e] text-center sm:text-sm lg:text-lg xl:text-xl'>Nombres</p>
-                <input className='mx-auto justify-center max-w-screen-sm rounded-full border-2 border-[#DB4437] text-[#ff5a23] text-center sm:text-sm lg:text-lg xl:text-xl' placeholder='Nombres' type='text' ></input>
+                <input className='mx-auto justify-center max-w-screen-sm rounded-full border-2 border-[#045042] text-[#ff5a23] text-center sm:text-sm lg:text-lg xl:text-xl' placeholder='Nombres' type='text' ></input>
                 <p className='mt-6 pt-2 text-[#03624e] text-center sm:text-sm lg:text-lg xl:text-xl'>Apellidos</p>
-                <input className='mx-auto justify-center max-w-screen-sm rounded-full border-2 border-[#DB4437] text-[#ff5a23] text-center sm:text-sm lg:text-lg xl:text-xl' placeholder='Apellidos' type='text'></input>
+                <input className='mx-auto justify-center max-w-screen-sm rounded-full border-2 border-[#045042] text-[#ff5a23] text-center sm:text-sm lg:text-lg xl:text-xl' placeholder='Apellidos' type='text'></input>
                 <p className='mt-6 pt-2 text-[#03624e] text-center sm:text-sm lg:text-lg xl:text-xl'>Tipo de Documento</p>
-                <input className='mx-auto justify-center max-w-screen-sm rounded-full border-2 border-[#DB4437] text-[#ff5a23] text-center sm:text-sm lg:text-lg xl:text-xl' placeholder='Tipo de documento' type='text'></input>
+                <input className='mx-auto justify-center max-w-screen-sm rounded-full border-2 border-[#045042] text-[#ff5a23] text-center sm:text-sm lg:text-lg xl:text-xl' placeholder='Tipo de documento' type='text'></input>
                 <p className='mt-6 pt-2 text-[#03624e] text-center sm:text-sm lg:text-lg xl:text-xl'>Documento</p>
-                <input className='mx-auto justify-center max-w-screen-sm rounded-full border-2 border-[#DB4437] text-[#ff5a23] text-center sm:text-sm lg:text-lg xl:text-xl' placeholder='Documento' type='text'></input>
+                <input className='mx-auto justify-center max-w-screen-sm rounded-full border-2 border-[#045042] text-[#ff5a23] text-center sm:text-sm lg:text-lg xl:text-xl' placeholder='Documento' type='text'></input>
                 <p className='mt-6 pt-2 text-[#03624e] text-center sm:text-sm lg:text-lg xl:text-xl'>Ciudad</p>
-                <input className='mx-auto justify-center max-w-screen-sm rounded-full border-2 border-[#DB4437] text-[#ff5a23] text-center sm:text-sm lg:text-lg xl:text-xl' placeholder='Ciudad' type='text'></input>
+                <input className='mx-auto justify-center max-w-screen-sm rounded-full border-2 border-[#045042] text-[#ff5a23] text-center sm:text-sm lg:text-lg xl:text-xl' placeholder='Ciudad' type='text'></input>
                 <p className='mt-6 pt-2 text-[#03624e] text-center sm:text-sm lg:text-lg xl:text-xl'>Municipio</p>
-                <input className='mx-auto justify-center max-w-screen-sm rounded-full border-2 border-[#DB4437] text-[#ff5a23] text-center sm:text-sm lg:text-lg xl:text-xl' placeholder='Municipio' type='text'></input>
+                <input className='mx-auto justify-center max-w-screen-sm rounded-full border-2 border-[#045042] text-[#ff5a23] text-center sm:text-sm lg:text-lg xl:text-xl' placeholder='Municipio' type='text'></input>
                 <p className='mt-6 pt-2 text-[#03624e] text-center sm:text-sm lg:text-lg xl:text-xl'>Dirección</p>
-                <input className='mx-auto justify-center max-w-screen-sm rounded-full border-2 border-[#DB4437] text-[#ff5a23] text-center sm:text-sm lg:text-lg xl:text-xl' placeholder='Dirección' type='text'></input>
+                <input className='mx-auto justify-center max-w-screen-sm rounded-full border-2 border-[#045042] text-[#ff5a23] text-center sm:text-sm lg:text-lg xl:text-xl' placeholder='Dirección' type='text'></input>
                 <p className='mt-6 pt-2 text-[#03624e] text-center sm:text-sm lg:text-lg xl:text-xl'>Teléfono</p>
-                <input className='mx-auto justify-center max-w-screen-sm rounded-full border-2 border-[#DB4437] text-[#ff5a23] text-center sm:text-sm lg:text-lg xl:text-xl' placeholder='Teléfono' type='number'></input>
+                <input className='mx-auto justify-center max-w-screen-sm rounded-full border-2 border-[#045042] text-[#ff5a23] text-center sm:text-sm lg:text-lg xl:text-xl' placeholder='Teléfono' type='number'></input>
                 <p className='mt-6 pt-2 text-[#03624e] text-center sm:text-sm lg:text-lg xl:text-xl'>Contraseña</p>
-                <input className='mx-auto justify-center max-w-screen-sm rounded-full border-2 border-[#DB4437] text-[#ff5a23] text-center sm:text-sm lg:text-lg xl:text-xl' placeholder='Contraseña' type='text'></input>
+                <input className='mx-auto justify-center max-w-screen-sm rounded-full border-2 border-[#045042] text-[#ff5a23] text-center sm:text-sm lg:text-lg xl:text-xl' placeholder='Contraseña' type='text'></input>
                 <p className='mt-6 pt-2 text-[#03624e] text-center sm:text-sm lg:text-lg xl:text-xl'>Repetir la contraseña</p>
-                <input className='mx-auto justify-center max-w-screen-sm rounded-full border-2 border-[#DB4437] text-[#ff5a23] text-center sm:text-sm lg:text-lg xl:text-xl' placeholder='Repetir Contraseña' type='text'></input>
+                <input className='mx-auto justify-center max-w-screen-sm rounded-full border-2 border-[#045042] text-[#ff5a23] text-center sm:text-sm lg:text-lg xl:text-xl' placeholder='Repetir Contraseña' type='text'></input>
 
                 <button className='mx-auto mt-6 mb-2 px-6 py-2 max-w-[240px] rounded-full bg-[#03624e] text-white'>ENVIAR</button>
 
